40911fce62ee4d606c9d9fa766980b1ca8821901
ackn14 <levia731.ky@gmail.com>
2022-08-06 18:14:47 +0900

    投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,20 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
+  end
+  # showアクションよりも上に書かないとエラーになる
+  def new
+    
+  end
+  def show
+    # パラメータ値を代入
+    id = params[:id]
+    @show = Post.find_by(id: id)
+  end
+  def create
+    # textareaタグのname属性に指定した値を引数に指定
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to('/posts/index')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'posts/index' => 'posts#index'
-  get "/" => "home#top"
+  # showアクションよりも上に書かないとエラーになる
+  get "posts/new" => "posts#new"
+  get 'posts/:id' => 'posts#show'
+  post 'posts/create' => 'posts#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # get "/" => "home#top"
+  get "/" => "home#top"
   get "about" => "home#about"
   # Defines the root path route ("/")
   # root "articles#index"
