a42f01d4e8ef321e0fc878b06a2048a945b0e80a
ackn14 <levia731.ky@gmail.com>
2022-08-06 16:46:12 +0900

    データベースの中身を表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_03_143033) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_06_070332) do
   create_table "posts", charset: "utf8mb3", force: :cascade do |t|
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
